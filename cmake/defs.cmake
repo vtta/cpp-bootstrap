43094ed73d323e2165bbdd36e87440e70bdcfb15
vtta <vtta0124@gmail.com>
2019-11-05 01:09:19 +0800

    Change to link main library statically

--- cmake/defs.cmake
+++ cmake/defs.cmake
@@ -2,20 +2,15 @@ set(CMAKE_MACOSX_RPATH OFF)
 
 macro (add_cpp_exec exec_name)
     add_executable ("${exec_name}" "${ARGN}")
-    target_link_libraries ("${exec_name}" ${CONAN_LIBS} cpp_shared)
+    target_link_libraries ("${exec_name}" ${CONAN_LIBS} src_static)
     add_custom_target(run-${exec_name} "${CMAKE_BINARY_DIR}/bin/${exec_name}")
     add_dependencies(run-${exec_name} ${exec_name})
 endmacro (add_cpp_exec)
 
 macro (add_gtest TESTNAME)
     add_executable (${TESTNAME} ${ARGN})
-    target_link_libraries (${TESTNAME} gtest gmock gtest_main ${CONAN_LIBS} cpp_shared)
+    target_link_libraries (${TESTNAME} gtest gmock gtest_main ${CONAN_LIBS} src_static)
     add_test (NAME ${TESTNAME} COMMAND ${TESTNAME})
-    set_target_properties (
-        ${TESTNAME}
-        PROPERTIES
-        RUNTIME_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/test"
-    )
     add_custom_target(run-${TESTNAME} "${CMAKE_BINARY_DIR}/bin/${TESTNAME}")
     add_dependencies(run-${TESTNAME} ${TESTNAME})
 endmacro (add_gtest)
